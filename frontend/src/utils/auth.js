26b015bc12c20bdd055d271fea3005418285fb73
Nikolay Saitov <saitovkms@gmail.com>
2022-07-21 18:24:11 +0300

    13

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,59 +1,53 @@
-// export const BASE_URL = 'https://saitovkmsapi.nomoredomains.xyz';
-
-export let BASE_URL = '';
+export let BASE_URL = "";
 const { NODE_ENV } = process.env;
-if ( NODE_ENV === 'production' ) {
-    BASE_URL = 'https://saitovkmsapi.nomoredomains.xyz';
+if (NODE_ENV === "production") {
+  BASE_URL = "https://saitovkmsapi.nomoredomains.xyz";
 } else {
-    BASE_URL = 'http://localhost:3000'
+  BASE_URL = "http://localhost:3000";
 }
 
-
-
-   
 function checkResponse(res) {
-    if (res.ok) {
-        return res.json();
-    }
-    return Promise.reject(res.status);
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(res.status);
 }
 
 export const register = (email, password) => {
-    return fetch(`${BASE_URL}/signup`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        })
-    }).then(checkResponse);
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  }).then(checkResponse);
 };
 
 export const authorize = (email, password) => {
-    return fetch(`${BASE_URL}/signin`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        })
-    }).then(checkResponse);
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  }).then(checkResponse);
 };
 
 export const checkToken = () => {
-    return fetch(`${BASE_URL}/users/me`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
-        }
-    })
-    .then(checkResponse);
-}
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+    },
+  }).then(checkResponse);
+};
