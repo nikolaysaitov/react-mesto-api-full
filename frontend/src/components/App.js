5ef422270023e5bb5558c7e254cc3ed410e6eff5
Nikolay Saitov <saitovkms@gmail.com>
2022-07-21 20:27:33 +0300

    23

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -126,7 +126,7 @@ function App() {
 
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
     const token = localStorage.getItem('jwt');
 
     // Отправляем запрос в API и получаем обновлённые данные карточки
