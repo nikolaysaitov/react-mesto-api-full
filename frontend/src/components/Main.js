6741225efe11d8432bbc2c8773714a8969034082
Nikolay Saitov <saitovkms@gmail.com>
2022-07-21 19:48:24 +0300

    19

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,14 +3,24 @@ import Card from "./Card.js";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import { useContext } from "react";
 
-function Main(props) {
+
+
+function Main({
+  onEditAvatar,
+  onEditProfile,
+  onAddPlace,
+  onCardClick,
+  cards,
+  onCardLike,
+  onCardDelete,
+}) {
   const currentUser = useContext(CurrentUserContext);
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__id">
-          <div className="profile__id-wrapper" onClick={props.onEditAvatar}>
+          <div className="profile__id-wrapper" onClick={onEditAvatar}>
             <img
               src={currentUser.avatar}
               className="profile__id-avatar"
@@ -24,7 +34,7 @@ function Main(props) {
               <button
                 className="profile__button-edit"
                 type="button"
-                onClick={props.onEditProfile}
+                onClick={onEditProfile}
               ></button>
             </div>
             <p className="profile__id-subtitle">{currentUser.about}</p>
@@ -33,19 +43,19 @@ function Main(props) {
         <button
           className="profile__button-add"
           type="button"
-          onClick={props.onAddPlace}
+          onClick={onAddPlace}
         ></button>
       </section>
       <section className="pictures">
         <ul className="pictures__board">
-          {props.cards.map((card) => {
+          {cards.map((card) => {
             return (
               <Card
                 key={card._id}
                 card={card}
-                onCardClick={props.onCardClick}
-                onCardLike={props.onCardLike}
-                onCardDelete={props.onCardDelete}
+                onCardClick={onCardClick}
+                onCardLike={onCardLike}
+                onCardDelete={onCardDelete}
               />
             );
           })}
@@ -56,67 +66,4 @@ function Main(props) {
 }
 
 export default Main;
-
-
-// function Main({
-//   onEditAvatar,
-//   onEditProfile,
-//   onAddPlace,
-//   onCardClick,
-//   cards,
-//   onCardLike,
-//   onCardDelete,
-// }) {
-//   const currentUser = useContext(CurrentUserContext);
-
-//   return (
-//     <main className="content">
-//       <section className="profile">
-//         <div className="profile__id">
-//           <div className="profile__id-wrapper" onClick={onEditAvatar}>
-//             <img
-//               src={currentUser.avatar}
-//               className="profile__id-avatar"
-//               alt="аватар"
-//             />
-//           </div>
-
-//           <div className="profile__id-info">
-//             <div className="profile__name">
-//               <h1 className="profile__id-title">{currentUser.name}</h1>
-//               <button
-//                 className="profile__button-edit"
-//                 type="button"
-//                 onClick={onEditProfile}
-//               ></button>
-//             </div>
-//             <p className="profile__id-subtitle">{currentUser.about}</p>
-//           </div>
-//         </div>
-//         <button
-//           className="profile__button-add"
-//           type="button"
-//           onClick={onAddPlace}
-//         ></button>
-//       </section>
-//       <section className="pictures">
-//         <ul className="pictures__board">
-//           {cards.map((card) => {
-//             return (
-//               <Card
-//                 key={card._id}
-//                 card={card}
-//                 onCardClick={onCardClick}
-//                 onCardLike={onCardLike}
-//                 onCardDelete={onCardDelete}
-//               />
-//             );
-//           })}
-//         </ul>
-//       </section>
-//     </main>
-//   );
-// }
-
-// export default Main;
 
