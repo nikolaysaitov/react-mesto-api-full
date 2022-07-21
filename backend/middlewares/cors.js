fb841347df64bc637953e7ec621dd00b37c07c91
Nikolay Saitov <saitovkms@gmail.com>
2022-07-21 17:41:41 +0300

    10

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,72 +1,44 @@
-// const allowedCors = [
-//   'localhost:3000',
-//   'http://localhost:3000',
-//   'http://saitovkms.nomoredomains.xyz/',
-//   'https://saitovkms.nomoredomains.xyz/',
-// ];
-// // eslint-disable-next-line consistent-return
-// const cors = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
-//   const requestHeaders = req.headers['access-control-request-headers'];
-
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     // res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.status(200).end();
-//   }
-//   next();
-// };
-// module.exports = { cors };
-
-
-const cors = require('cors');
-
-const allowedCors = {
-  origin: [
-    'https://saitovkms.nomoredomains.xyz/',
-    'http://saitovkms.nomoredomains.xyz/',
-    'http://localhost:3000',
-    'http://localhost:3001',
-  ],
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-};
-
-module.exports = cors(allowedCors);
-
-
-
-
 
 // const cors = require('cors');
 
-// const listUrl = () => {
-//   const { NODE_ENV } = process.env;
-//   let list = [];
-//   if (NODE_ENV) {
-//     list = [
-//       'https://saitovkms.nomoredomains.xyz/',
-//     ];
-//   } else {
-//     list = [
-//       'http://localhost:3000',
-//       'http://localhost:3001',
-//     ];
-//   }
-//   return list;
-// };
-
 // const allowedCors = {
-//   origin: listUrl(),
+//   origin: [
+//     'https://saitovkms.nomoredomains.xyz/',
+//     'http://saitovkms.nomoredomains.xyz/',
+//     'http://localhost:3000',
+//     'http://localhost:3001',
+//   ],
 //   credentials: true,
 //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 // };
 
 // module.exports = cors(allowedCors);
+
+
+const allowedCors = [
+  'https://saitovkms.nomoredomains.xyz/',
+  'http://saitovkms.nomoredomains.xyz/',
+  'localhost:3000',
+  'http://localhost:3000',
+];
+
+// eslint-disable-next-line consistent-return
+module.exports = ((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).end();
+  }
+
+  next();
+});
