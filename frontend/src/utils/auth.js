4afcaf4494f000b640614f27581a4eff5fc2b058
Nikolay Saitov <saitovkms@gmail.com>
2022-07-21 18:11:03 +0300

    11

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -19,7 +19,7 @@ function checkResponse(res) {
 }
 
 export const register = (email, password) => {
-    return fetch(`${BASE_URL}signup`, {
+    return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -33,7 +33,7 @@ export const register = (email, password) => {
 };
 
 export const authorize = (email, password) => {
-    return fetch(`${BASE_URL}signin`, {
+    return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -47,7 +47,7 @@ export const authorize = (email, password) => {
 };
 
 export const checkToken = () => {
-    return fetch(`${BASE_URL}users/me`, {
+    return fetch(`${BASE_URL}/users/me`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
