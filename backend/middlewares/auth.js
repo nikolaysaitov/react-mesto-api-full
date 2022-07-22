0a4f0d07cf7d7f12001b9ca379211335d5772594
Nikolay Saitov <saitovkms@gmail.com>
2022-07-22 12:55:36 +0300

    token2

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken');
-const AuthorisationError = require('../errors/authorisation_error_401');
+const jwt = require("jsonwebtoken");
+const AuthorisationError = require("../errors/authorisation_error_401");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  const { JWT_SECRET = 'some-secret-key' } = process.env;
+  const { JWT_SECRET = "some-secret-key" } = process.env;
 
-  if (!authorization || !authorization.startsWith('Bearer')) {
-    return next(new AuthorisationError('Необходима авторизация'));
+  if (!authorization || !authorization.startsWith("Bearer")) {
+    return next(new AuthorisationError("Необходима авторизация"));
   }
 
-  const token = authorization.replace('Bearer ', '');
+  // const token = authorization.replace("Bearer ", "");
+  const token = authorization.replace(/^\S+/, '').trim();
   let payload;
-  
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw new AuthorisationError('Необходима авторизация');
+    throw new AuthorisationError("Необходима авторизация");
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
