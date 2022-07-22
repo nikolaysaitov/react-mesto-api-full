0ac8d24b2386c2a838b20ec53d30acbef00a8ddf
Nikolay Saitov <saitovkms@gmail.com>
2022-07-22 12:46:46 +0300

    token

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -6,104 +6,206 @@ class Api {
 
   _getResponseData(res) {
     if (!res.ok) {
-      return Promise.reject(`Ошибка: ${res.status}`);
+        return Promise.reject(`Ошибка: ${res.status}`);
     }
     return res.json();
-  }
+}
 
-  getProfile(token) {
+  getProfile() {
     return fetch(`${this._baseUrl}/users/me`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._getResponseData);
+      headers: this._headers,
+    })
+      .then(this._getResponseData)
+      
   }
 
-  getInitialCards(token) {
+  getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._getResponseData);
+      headers: this._headers,
+    })
+    .then(this._getResponseData)
+      
   }
 
-  editProfile(name, about, token) {
+  editProfile(name, about) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
+      headers: this._headers,
       body: JSON.stringify({
         name,
         about,
       }),
-    }).then(this._getResponseData);
+    })
+    .then(this._getResponseData)
+    
   }
-  addCard(name, link, token) {
+  addCard(name, link) {
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
+      headers: this._headers,
       body: JSON.stringify({
         name,
         link,
       }),
-    }).then(this._getResponseData);
+    })
+    .then(this._getResponseData)
+    
   }
-  deleteCard(id, token) {
+  deleteCard(id) {
     return fetch(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._getResponseData);
+      headers: this._headers
+    })
+    .then(this._getResponseData)
+      
   }
 
-  deleteLike(id, token) {
+  deleteLike(id) {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._getResponseData);
+      headers: this._headers
+    })
+    .then(this._getResponseData)
+      
   }
 
-  addLike(id, token) {
+  addLike(id) {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(this._getResponseData);
+      headers: this._headers
+    })
+    .then(this._getResponseData)
+    
   }
 
-  updateAvatar(avatar, token) {
+  
+  updateAvatar(avatar) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
+      headers: this._headers,
       body: JSON.stringify({
-        avatar,
-      }),
-    }).then(this._getResponseData);
+        avatar
+      })
+    })
+    .then(this._getResponseData)
+    
   }
+  
 
   // другие методы работы с API
 }
-
 export const api = new Api({
   baseUrl: "https://saitovkmsapi.nomoredomains.xyz",
   headers: {
     "Content-Type": "application/json",
   },
 });
+// class Api {
+//   constructor({ baseUrl, headers }) {
+//     this._headers = headers;
+//     this._baseUrl = baseUrl; // тело конструктора
+//   }
+
+//   _getResponseData(res) {
+//     if (!res.ok) {
+//       return Promise.reject(`Ошибка: ${res.status}`);
+//     }
+//     return res.json();
+//   }
+
+//   getProfile(token) {
+//     return fetch(`${this._baseUrl}/users/me`, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }).then(this._getResponseData);
+//   }
+
+//   getInitialCards(token) {
+//     return fetch(`${this._baseUrl}/cards`, {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }).then(this._getResponseData);
+//   }
+
+//   editProfile(name, about, token) {
+//     return fetch(`${this._baseUrl}/users/me`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({
+//         name,
+//         about,
+//       }),
+//     }).then(this._getResponseData);
+//   }
+//   addCard(name, link, token) {
+//     return fetch(`${this._baseUrl}/cards`, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({
+//         name,
+//         link,
+//       }),
+//     }).then(this._getResponseData);
+//   }
+//   deleteCard(id, token) {
+//     return fetch(`${this._baseUrl}/cards/${id}`, {
+//       method: "DELETE",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }).then(this._getResponseData);
+//   }
+
+//   deleteLike(id, token) {
+//     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+//       method: "DELETE",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }).then(this._getResponseData);
+//   }
+
+//   addLike(id, token) {
+//     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+//       method: "PUT",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     }).then(this._getResponseData);
+//   }
+
+//   updateAvatar(avatar, token) {
+//     return fetch(`${this._baseUrl}/users/me/avatar`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify({
+//         avatar,
+//       }),
+//     }).then(this._getResponseData);
+//   }
+
+//   // другие методы работы с API
+// }
+
+// export const api = new Api({
+//   baseUrl: "https://saitovkmsapi.nomoredomains.xyz",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
