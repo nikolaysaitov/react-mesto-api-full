62f1623b5f615045731d6105b5402bc0c02d4642
Nikolay Saitov <saitovkms@gmail.com>
2022-07-22 13:34:11 +0300

    hjhj

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,3 +1,115 @@
+// class Api {
+//   constructor({ baseUrl, headers }) {
+//     this._headers = headers;
+//     this._baseUrl = baseUrl; // тело конструктора
+//   }
+
+//   _getResponseData(res) {
+//     if (!res.ok) {
+//         return Promise.reject(`Ошибка: ${res.status}`);
+//     }
+//     return res.json();
+// }
+
+//   getProfile() {
+//     return fetch(`${this._baseUrl}/users/me`, {
+//       method: 'GET',
+//       credentials: 'include',
+//       headers: this._headers
+//     })
+//       .then(this._getResponseData)
+      
+//   }
+
+//   getInitialCards() {
+//     return fetch(`${this._baseUrl}/cards`, {
+//       method: 'GET',
+//       credentials: 'include',
+//       headers: this._headers
+//     })
+//     .then(this._getResponseData)
+      
+//   }
+
+//   editProfile(name, about) {
+//     return fetch(`${this._baseUrl}/users/me`, {
+//       method: "PATCH",
+//       credentials: 'include',
+//       headers: this._headers,
+//       body: JSON.stringify({
+//         name,
+//         about,
+//       }),
+//     })
+//     .then(this._getResponseData)
+    
+//   }
+//   addCard(name, link) {
+//     return fetch(`${this._baseUrl}/cards`, {
+//       method: "POST",
+//       credentials: 'include',
+//       headers: this._headers,
+//       body: JSON.stringify({
+//         name,
+//         link,
+//       }),
+//     })
+//     .then(this._getResponseData)
+    
+//   }
+//   deleteCard(id) {
+//     return fetch(`${this._baseUrl}/cards/${id}`, {
+//       method: "DELETE",
+//       credentials: 'include',
+//       headers: this._headers
+//     })
+//     .then(this._getResponseData)
+      
+//   }
+
+//   deleteLike(id) {
+//     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+//       method: "DELETE",
+//       credentials: 'include',
+//       headers: this._headers
+//     })
+//     .then(this._getResponseData)
+      
+//   }
+
+//   addLike(id) {
+//     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+//       method: "PUT",
+//       credentials: 'include',
+//       headers: this._headers
+//     })
+//     .then(this._getResponseData)
+    
+//   }
+
+  
+//   updateAvatar(avatar) {
+//     return fetch(`${this._baseUrl}/users/me/avatar`, {
+//       method: "PATCH",
+//       credentials: 'include',
+//       headers: this._headers,
+//       body: JSON.stringify({
+//         avatar
+//       })
+//     })
+//     .then(this._getResponseData)
+    
+//   }
+  
+
+//   // другие методы работы с API
+// }
+// export const api = new Api({
+//   baseUrl: "https://saitovkmsapi.nomoredomains.xyz",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 class Api {
   constructor({ baseUrl, headers }) {
     this._headers = headers;
@@ -11,26 +123,30 @@ class Api {
     return res.json();
   }
 
-  getProfile() {
+  getProfile(token) {
     return fetch(`${this._baseUrl}/users/me`, {
-      method: "GET",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then(this._getResponseData);
   }
 
-  getInitialCards() {
+  getInitialCards(token) {
     return fetch(`${this._baseUrl}/cards`, {
-      method: "GET",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then(this._getResponseData);
   }
 
   editProfile(name, about, token) {
     return fetch(`${this._baseUrl}/users/me`, {
+      credentials: 'include',
       method: "PATCH",
-      credentials: "include",
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
@@ -41,46 +157,61 @@ class Api {
       }),
     }).then(this._getResponseData);
   }
-  addCard(name, link) {
+  addCard(name, link, token) {
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       body: JSON.stringify({
         name,
         link,
       }),
     }).then(this._getResponseData);
   }
-  deleteCard(id) {
+  deleteCard(id, token) {
     return fetch(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then(this._getResponseData);
   }
 
-  deleteLike(id) {
+  deleteLike(id, token) {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "DELETE",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then(this._getResponseData);
   }
 
-  addLike(id) {
+  addLike(id, token) {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "PUT",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }).then(this._getResponseData);
   }
 
-  updateAvatar(avatar) {
+  updateAvatar(avatar, token) {
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
-      credentials: "include",
-      headers: this._headers,
+      credentials: 'include',
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       body: JSON.stringify({
         avatar,
       }),
@@ -89,12 +220,15 @@ class Api {
 
   // другие методы работы с API
 }
+
 export const api = new Api({
   baseUrl: "https://saitovkmsapi.nomoredomains.xyz",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+
 // class Api {
 //   constructor({ baseUrl, headers }) {
 //     this._headers = headers;
