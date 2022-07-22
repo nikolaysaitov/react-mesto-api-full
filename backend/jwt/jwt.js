492dea6081e99f09f381d9bee7f0917627f05712
Nikolay Saitov <saitovkms@gmail.com>
2022-07-22 12:10:27 +0300

    token

--- backend/jwt/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
-
-const SECRET_KEY = 'very-secret';
-const checkToken = (token) => jwt.verify(token, NODE_ENV ? JWT_SECRET : SECRET_KEY);
-
-module.exports = { checkToken };
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,11 +1,9 @@
-
-
-const { checkToken } = require('../jwt/jwt');
+const jwt = require('jsonwebtoken');
 const AuthorisationError = require('../errors/authorisation_error_401');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
+  const { JWT_SECRET = 'some-secret-key' } = process.env;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new AuthorisationError('Необходима авторизация'));
@@ -16,7 +14,7 @@ module.exports = (req, res, next) => {
   
 
   try {
-    payload = checkToken(token);
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new AuthorisationError('Необходима авторизация');
   }
@@ -25,28 +23,3 @@ module.exports = (req, res, next) => {
 
   return next(); // пропускаем запрос дальше
 };
-// const jwt = require('jsonwebtoken');
-// const AuthorisationError = require('../errors/authorisation_error_401');
-
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   const { JWT_SECRET = 'some-secret-key' } = process.env;
-
-//   if (!authorization || !authorization.startsWith('Bearer')) {
-//     return next(new AuthorisationError('Необходима авторизация'));
-//   }
-
-//   const token = authorization.replace('Bearer ', '');
-//   let payload;
-  
-
-//   try {
-//     payload = jwt.verify(token, JWT_SECRET);
-//   } catch (err) {
-//     throw new AuthorisationError('Необходима авторизация');
-//   }
-
-//   req.user = payload; // записываем пейлоуд в объект запроса
-
-//   return next(); // пропускаем запрос дальше
-// };
